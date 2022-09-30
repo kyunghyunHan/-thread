110b6aa851ffbd5204108bae42e4098b28da6cb2
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-09-30 14:17:16 +0900

    ㅁㅇㅇ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {} from the 생성된 스레드!", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+
+    // for i in 1..5 {
+    //     println!("hi number {} from the 메인 스레드!", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+    use std::sync::mpsc;
+
+    // handle.join().unwrap();
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
